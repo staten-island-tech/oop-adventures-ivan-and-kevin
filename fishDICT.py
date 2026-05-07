f1569bed0f7d316e066608738588e8918081478d
ivanyeung43 <ivany43@nycstudents.net>
2026-05-07 08:43:39 -0400

    a

--- fishDICT.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-response = requests.get("https://fishbase.ropensci.org/fishbase")
-if response.status_code != 200:
-    print("Error fetching data!")
-data = response.json()
-print(data)
-
