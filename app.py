e4e78898ceaa7fc88064de46b09dd5f4aa2c6f41
Kevinz297 <Kevinz297@nycstudents.net>
2026-05-07 09:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ class Fisherman:
     def sell_fish(self, fish_sold, money):
         self.money += money
 
+
     def buy_equipment(self, cost, rod, bait):
         if self.money >= cost:
             self.money -= cost
@@ -21,3 +22,29 @@ class Fisherman:
             print(f"{self.name} has bought {bait}.")
         else:
             print(f"{self.name} does not have enough money to buy the equipment.")
+
+    print=input("Enter the name of the fisherman: ")
+    Fisherman = Fisherman(name, money=100, experience=0, inventory=[])
+
+
+    while True:
+        action = input("What would you like to do? (fish/sell/buy/exit): ")
+        if action == "fish":
+            experience_gained = int(input("Enter the experience gained from fishing: "))
+            fish_caught = int(input("Enter the number of fish caught: "))
+            Fisherman.fish(experience_gained, fish_caught)
+        elif action == "sell":
+            fish_sold = int(input("Enter the number of fish sold: "))
+            money = int(input("Enter the amount of money earned from selling the fish: "))
+            Fisherman.sell_fish(fish_sold, money)
+        elif action == "buy":
+            print ("You can buy the following equipment: Rods or Bait:")
+            cost = int(input("Enter the cost of the equipment: "))
+            rod = input("Enter the type of rod you want to buy: ")
+            bait = input("Enter the type of bait you want to buy: ")
+            Fisherman.buy_equipment(cost, rod, bait)
+        elif action == "exit":
+            print(f"{Fisherman.name} has {Fisherman.money} money and {Fisherman.experience} experience points.")
+            break
+        else:
+            print("Invalid action. Please try again.")
