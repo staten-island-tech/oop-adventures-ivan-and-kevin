9e06281d5a9481a1ae1fc13081469642ac087085
ivanyeung43 <ivany43@nycstudents.net>
2026-05-11 09:25:25 -0400

    a

---
+++ lootTABLE.py
@@ -0,0 +1,14 @@
+import json
+fishDICT = open("./fishDICT.json", encoding="utf8")
+data = json.load(fishDICT)
+
+import random
+class calculator():
+    def mult(x,y):
+        print(x*y)
+        return(x*y)
+    def prob(x):
+        print(x/1)
+
+def probCAL():
+    
