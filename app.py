f9822a38b75c3dd1f14f5bd5a6fd21ce753e8423
Kevinz297 <Kevinz297@nycstudents.net>
2026-05-13 08:54:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-import json
-FishItem = open("./BuyDICT.json", encoding="utf-8")
-data = json.load(FishItem)
-FishItem.close()
 
-def item(rod, bait):
-    item=input("What item do you want to buy?").lower()
-    for item in data:
-        if item("rod"):
-            print("Rod List:")
-        elif item("bait"):
-            print("Bait List:")
